6c04f5e893a4ef7c505b40c357fc3f0cf06946a1
aca16atc <atchapman1@sheffield.ac.uk>
2018-02-22 20:35:13 +0000

    Can now select an asset manager from dropdown

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,7 +1,8 @@
 class AssetsController < ApplicationController
+  before_action :set_asset, only: [:show, :edit, :update, :destroy]
+
   def index
     @assets = Asset.all
-    render layout: 'application'
   end
 
   # GET /assets/new
@@ -9,19 +10,43 @@ class AssetsController < ApplicationController
     @asset = Asset.new
   end
 
+  # GET /assets/1/edit
+  def edit
+  end
+
   # POST /assets
   def create
     @asset = Asset.new(user_params)
 
     if @asset.save
-      redirect_to @asset, notice: 'User was successfully created.'
+      redirect_to @asset, notice: 'Asset was successfully created.'
     else
       render :new
     end
   end
 
-  # Only allow a trusted parameter "white list" through.
-  def user_params
-  params.require(:asset).permit(:forename,:surname,:password,:email,:phone,:department,:permission)
+  # PATCH/PUT /assets/1
+  def update
+    if @asset.update(asset_params)
+      redirect_to @asset, notice: 'Asset was successfully updated.'
+    else
+      render :edit
+    end
   end
+
+  # DELETE /assets/1
+  def destroy
+    @user.destroy
+    redirect_to assets_url, notice: 'Asset was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @asset = Asset.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :users
   resources :bookings
+  resources :assets
+
   match "/403", to: "errors#error_403", via: :all
   match "/404", to: "errors#error_404", via: :all
   match "/422", to: "errors#error_422", via: :all
@@ -13,6 +15,5 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :login
-  resources :assets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
