042ef7e7918b6e0d55461c22b99d6c56ba030353
Juneezee <zjeng1@sheffield.ac.uk>
2018-03-21 20:42:34 +0000

    Update helpers.rb

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,20 +1,20 @@
 module Helpers
-  def sign_in_as_zerjun_uid
+  def sign_in_using_uid
     fill_in 'user_username', with: ''
     fill_in 'user_password', with: ''
   end
 
-  def sign_in_as_zerjun_email
+  def sign_in_using_email
     fill_in 'user_username', with: ''
     fill_in 'user_password', with: ''
   end
 
-  def sign_in_as_wrong_username
+  def sign_in_using_wrong_username
     fill_in 'user_username', with: 'wrongusername'
     fill_in 'user_password', with: 'correctpassword'
   end
 
-  def sign_in_as_wrong_password
+  def sign_in_using_wrong_password
     fill_in 'user_username', with: 'zjeng1@sheffield.ac.uk'
     fill_in 'user_password', with: 'wrongpassword'
   end
