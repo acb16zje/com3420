a0346d100ff0076c5c96bc0c6191b4bf735ff830
Wei <wkkhaw1@sheffield.ac.uk>
2018-04-02 00:04:37 +0100

    Fixed category testing

--- spec/features/3_managing_categories_spec.rb
+++ spec/features/3_managing_categories_spec.rb
@@ -33,27 +33,35 @@ describe 'Managing categories' do
   end
 
   specify 'I can update a category' do
-    fill_in 'category_name', with: 'Cameras'
-    click_button('Create category')
-    expect(page).to have_content 'Category was successfully created.'
-    expect(page).to have_content 'Cameras'
+    FactoryBot.create :laptop_category
+    visit '/categories'
+    expect(page).to have_content 'Laptops'
     click_link('Edit')
-    expect(page).to have_content 'Editing Cameras'
-    fill_in 'category_name', with: 'Camera'
+    expect(page).to have_content 'Editing Laptops'
+    fill_in 'category_name', with: 'Cameras'
     click_button('Save changes')
     expect(page).to have_content 'Category was successfully updated'
-    expect(page).to have_content 'Camera'
-    expect(page).to_not have_content 'Cameras'
+    expect(page).to have_content 'Cameras'
+    expect(page).to_not have_content 'Laptops'
   end
 
-  specify 'I can delete a category' do
-    fill_in 'category_name', with: 'Cameras'
-    click_button('Create category')
-    expect(page).to have_content 'Category was successfully created.'
+  specify 'I can delete a category that no asset is using' do
+    FactoryBot.create :camera_category
+    visit '/categories'
     expect(page).to have_content 'Cameras'
     click_link('Delete')
     expect(page).to have_content 'Category was successfully deleted.'
     expect(page).to_not have_content 'Cameras'
+  end
+
+  specify 'I cannot delete a category that an asset is using' do
+    FactoryBot.create :camera_category
+    visit '/categories'
+    expect(page).to have_content 'Cameras'
+    FactoryBot.create :gopro
+    click_link('Delete')
+    expect(page).to have_content 'Cannot delete category because it is currently in use for an asset.'
+    expect(page).to have_content 'Cameras'
   end
 
   specify 'I can add favourite category to home page' do
@@ -88,4 +96,35 @@ describe 'Managing categories' do
     click_link('Remove')
     expect(page).to have_content 'Favourite category successfully removed'
   end
+
+  specify 'I can delete a category that a user is using for a favourite, but no asset is using' do
+    FactoryBot.create :camera_category
+    visit '/categories'
+    expect(page).to have_content 'Cameras'
+    visit '/'
+    expect(page).to_not have_content 'View Cameras'
+    click_link('Add Favourite')
+    expect(page).to have_content 'Add Favourite Category'
+    select('Cameras', from: 'user_home_category_category_id')
+    click_button('Add Category')
+    expect(page).to have_content 'View Cameras'
+    visit '/categories'
+    click_link('Delete')
+    expect(page).to have_content 'Category was successfully deleted.'
+    expect(page).to_not have_content 'Cameras'
+  end
+
+  specify 'I can view all categories in the filter tab' do
+    FactoryBot.create :camera_category
+    visit '/categories'
+    expect(page).to have_content 'Cameras'
+    FactoryBot.create :laptop_category
+    visit '/categories'
+    expect(page).to have_content 'Laptops'
+    visit '/categories/filter'
+    expect(page).to have_content 'Laptops'
+    expect(page).to have_content 'Cameras'
+  end
+
+
 end
