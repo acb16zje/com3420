f90f5bc16682cc598db921ce9181b9669dee76bd
aca16atc <atchapman1@sheffield.ac.uk>
2018-04-11 16:46:34 +0100

    Booking test for no peripherals now creates a booking for a date after now

--- spec/features/5_managing_bookings_spec.rb
+++ spec/features/5_managing_bookings_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
+require 'spec_helper'
 
 describe 'Managing bookings', js: true do
+
   before :each do
-    FactoryBot.create :laptop_category
+    FactoryBot.create :camera_category
     FactoryBot.create :gopro
     visit '/items/1/bookings/new'
-    #expect(page).to have_content 'Sign in'
-    #click_button 'Sign in'
-    sign_in_using_uid
-    click_button 'Sign in'
     expect(page).to have_content 'Create booking on GoPro Hero 5'
   end
 
   specify 'I can create a booking without time conflict' do
-    page.execute_script("$('#startDate').val('2 April 2018')")
-    page.execute_script("$('#startTime').val('9:00 AM')")
+    test_booking_date_start = DateTime.tomorrow.change({hour: 9, min: 0})
+    test_booking_date_end = test_booking_date_start + 1.days
+    page.execute_script("$('#startDate').val('#{test_booking_date_start.strftime("%d %B %Y")}')")
+    page.execute_script("$('#startTime').val('#{test_booking_date_start.strftime("%I:%M %p")}')")
     page.execute_script("$('#endDate').prop('disabled', false)")
     page.execute_script("$('#endTime').prop('disabled', false)")
-    page.execute_script("$('#endDate').val('3 April 2018')")
-    page.execute_script("$('#endTime').val('12:30 PM')")
+    page.execute_script("$('#endDate').val('#{test_booking_date_end.strftime("%d %B %Y")}')")
+    page.execute_script("$('#endTime').val('#{test_booking_date_end.strftime("%I:%M %p")}')")
     fill_in 'booking_next_location', with: 'pam liversidge building'
     fill_in 'booking_reason', with: 'Filming documentary'
     click_button('Confirm booking')
