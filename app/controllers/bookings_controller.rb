3a2d66073c11ad1f3a3df7df499fd1774dfbd10c
Juneezee <engzerjun@gmail.com>
2018-05-22 21:21:39 +0100

    This should fix the empty combined booking bug

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -155,21 +155,26 @@ class BookingsController < ApplicationController
         end
       end
 
-      # Check if all items on booking are the current user.
-      if (combined_booking.bookings.all? { |b| b.status == 2})
-        combined_booking.status = 2
-        combined_booking.save
+      if combined_booking.bookings.blank?
+        combined_booking.destroy
+        redirect_to bookings_path, notice: 'Error occurred in the booking process. Please try again.'
+      else
+        # Check if all items on booking are the current user.
+        if (combined_booking.bookings.all? { |b| b.status == 2})
+          combined_booking.status = 2
+          combined_booking.save
+        end
+
+        # Create notifications and send out the required emails
+        Notification.create(recipient: item.user, action: 'requested', notifiable: @booking, context: 'AM')
+        UserMailer.user_booking_requested(combined_booking).deliver
+
+        combined_booking.sorted_bookings.each do |m|
+          UserMailer.manager_booking_requested(m).deliver
+        end
+
+        redirect_to bookings_path, notice: 'Booking was successfully created.'
       end
-
-      # Create notifications and send out the required emails
-      Notification.create(recipient: item.user, action: 'requested', notifiable: @booking, context: 'AM')
-      UserMailer.user_booking_requested(combined_booking).deliver
-
-      combined_booking.sorted_bookings.each do |m|
-        UserMailer.manager_booking_requested(m).deliver
-      end
-
-      redirect_to bookings_path, notice: 'Booking was successfully created.'
     else
       redirect_to new_item_booking_path(item_id: @booking.item_id), alert: 'Chosen timeslot conflicts with other bookings.'
     end
